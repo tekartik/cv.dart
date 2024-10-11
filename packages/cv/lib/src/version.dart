e65e86bbd3d94b68870c5cd18e01daa8e63fdb0a
Alexandre Roux <alex@tekartik.com>
2024-10-11 14:20:33 +0200

    cv_v1.1.1

---
+++ packages/cv/lib/src/version.dart
@@ -0,0 +1,8 @@
+// ignore: depend_on_referenced_packages
+import 'package:pub_semver/pub_semver.dart';
+
+/// Package version text
+const packageVersionText = '1.1.1';
+
+/// Package version
+final packageVersion = Version.parse(packageVersionText);
